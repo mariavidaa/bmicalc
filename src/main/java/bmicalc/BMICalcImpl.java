db2d102ff8e047ce3c9510444bb4497c63ae7869
mariavidaa <mariavida262001@uma.es>
2023-02-19 01:37:37 +0100

    implementation category function

--- src/main/java/bmicalc/BMICalcImpl.java
+++ src/main/java/bmicalc/BMICalcImpl.java
@@ -7,15 +7,25 @@ public class BMICalcImpl implements BMICalc {
 	}
 
 	public double bmi(double mass, double height) {
-	/**
-	* Calculate the Body Mass Index (BMI) of a person.
-	* BMI = mass (kg) / height^2 (m)
-	*/
 		return Math.round(mass/(height*height));
 	}
 
 	public String category(double bmi) {
-		return "";
+		String category = null;
+		if (bmi < 18.5) {
+			category = "UNDERWEIGHT";
+		}
+		else if (bmi >= 18.5 && bmi <=24.9) {
+			category = "NORMAL";
+		}
+		else if (bmi >= 25.0 && bmi <=29.9) {
+			category = "OVERWEIGHT";
+		}
+		else if (bmi >=30) {
+			category = "OBESE";
+		}
+		
+		return category;
 	}
 
 	public boolean abdominalObesity(double waistCircumference, char gender) {
