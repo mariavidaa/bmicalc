447785f961be5d1711b0b26b4071c1e57341ace1
mariavidaa <mariavida262001@uma.es>
2023-04-26 23:30:45 +0200

    dividing interface

--- bmicalcproyect/src/main/java/bmicalc/CardioVascularMetrics.java
+++ bmicalcproyect/src/main/java/bmicalc/CardioVascularMetrics.java
@@ -13,26 +13,5 @@ public interface CardioVascularMetrics {
 	 */
 	public double calculateBodyMassIndex(double mass, double height);
 	
-	/**
-	 * Categorize a person according to the BMI category:
-	 *   - UNDERWEIGHT for a BMI below 18.5
-	 *   - NORMAL for a BMI between 18.5 and 24.9
-	 *   - OVERWEIGHT for a BMI between 25.0 and 29.9
-	 *   - OBESE for a BMI above or equal 30.0
-	 *   
-	 * @param bmi Body Mass Index (BMI) value.
-	 * @return	  String representing the BMI category.
-	 */
-	public ObesityCategory getObesityCategory(double bmi);
 	
-	/**
-	 * Determine whether a person has abdominal obesity.
-	 * A woman has abdominal obesity if her waist circumference is greater than 80 cm.
-	 * A man has abdominal obesity if his waist circumference is greater than 90 cm.
-	 * 
-	 * @param waistCircumference	Waist circumference.
-	 * @param gender				Male ('M') or Female ('F').
-	 * @return						True if the person has abdominal obesity, false otherwise.
-	 */
-	public boolean abdominalObesity(double waistCircumference, char gender);
 }
